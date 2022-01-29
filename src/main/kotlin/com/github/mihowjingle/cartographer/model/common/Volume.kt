8681bf303efff936787d394830c78b544c576451
Michał <michal.dziegiel91@gmail.com>
2022-01-29 21:50:44 +0100

    Research

--- src/main/kotlin/com/github/mihowjingle/cartographer/model/common/Volume.kt
+++ src/main/kotlin/com/github/mihowjingle/cartographer/model/common/Volume.kt
@@ -1,7 +1,8 @@
 package com.github.mihowjingle.cartographer.model.common
 /**
  * Yes, this basically [Position], but... names matter (tm)
- * z = height!
+ * The numbers are like radius rather than diameter, for example z of 60_000 means 120_000 total height.
+ * 60km above and 60km below the sensor manager plane
  */
 // todo which is width, which is depth? also would be nice to relate to the 0, 90, 180, 270 markers
 data class Volume(val x: Number, val z: Number, val y: Number)
