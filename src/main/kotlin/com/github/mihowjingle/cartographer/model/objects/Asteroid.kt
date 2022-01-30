458eeb45ed83c75e9af3c3adc211ca715616c623
Michał <michal.dziegiel91@gmail.com>
2022-01-30 15:31:01 +0100

    Some research on Asteroid

--- src/main/kotlin/com/github/mihowjingle/cartographer/model/objects/Asteroid.kt
+++ src/main/kotlin/com/github/mihowjingle/cartographer/model/objects/Asteroid.kt
@@ -11,12 +11,13 @@ data class Asteroid(val type: Type, val position: Position, val percentOfDefault
         ASTEROID_3(label = "Asteroid_3", defaultRus = 9000, maxSupportedCollectors = 1),
         ASTEROID_4(label = "Asteroid_4", defaultRus = 18000, maxSupportedCollectors = 2),
         ASTEROID_5(label = "Asteroid_5", defaultRus = 40000, maxSupportedCollectors = 3),
+        ASTEROID_6(label = "Asteroid_6", defaultRus = 0, maxSupportedCollectors = 0),
 
-        // found in existing HW1 maps TODO! research maxSupportedCollectors, 1 is just a placeholder
+        // found in existing HW1 maps
         ASTEROID_1_MP(label = "Asteroid1_MP", defaultRus = 2400, maxSupportedCollectors = 1),
         ASTEROID_2_MP(label = "Asteroid2_MP", defaultRus = 3600, maxSupportedCollectors = 1),
-        ASTEROID_3_MP(label = "Asteroid3_MP", defaultRus = 4800, maxSupportedCollectors = 1),
-        ASTEROID_4_MP(label = "Asteroid4_MP", defaultRus = 6000, maxSupportedCollectors = 1);
+        ASTEROID_3_MP(label = "Asteroid3_MP", defaultRus = 4800, maxSupportedCollectors = 2),
+        ASTEROID_4_MP(label = "Asteroid4_MP", defaultRus = 6000, maxSupportedCollectors = 3);
 
         // todo probably more, trial-error test in-game and research resources etc.
 
