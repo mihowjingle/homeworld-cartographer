e72250f6ff6aefef86844066b0238b53288155cd
Michał <michal.dziegiel91@gmail.com>
2022-01-30 19:43:28 +0100

    Research on Pebble (probably) done

--- src/main/kotlin/com/github/mihowjingle/cartographer/model/objects/Pebble.kt
+++ src/main/kotlin/com/github/mihowjingle/cartographer/model/objects/Pebble.kt
@@ -3,7 +3,8 @@ package com.github.mihowjingle.cartographer.model.objects
 import com.github.mihowjingle.cartographer.model.common.Position
 
 /**
- * "Pebble_3" crashes the game, allegedly
+ * "Pebble_3" crashes the game, allegedly.
+ * After some investigation, last 3 numbers (in .level file) DO NOT seem to represent orientation. But what?
  */
 data class Pebble(val type: Type, val position: Position) : Entity {
     enum class Type(val label: String) {
@@ -17,6 +18,5 @@ data class Pebble(val type: Type, val position: Position) : Entity {
     val z by position::z
     val y by position::y
 
-    // todo last 3 numbers...? rotation?
     override fun toLua() = "addPebble(\"${type.label}\", {${position.x}, ${position.z}, ${position.y}}, 0.0, 0.0, 0.0)"
 }
